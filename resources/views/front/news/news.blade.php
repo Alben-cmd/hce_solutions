d4fa995e5222bc39ab59533addaafa88d17a14c9
Alben-cmd <alenoghenabenjamin@gmail.com>
2024-11-22 08:32:46 +0100

    updates

--- resources/views/front/news/news.blade.php
+++ resources/views/front/news/news.blade.php
@@ -22,13 +22,52 @@
     <!-- Alt Services Section -->
             <!-- Section Title -->
         
-    <section id="alt-services" class="alt-services section">
-        <div class="container section-title" data-aos="fade-up">
-            <!-- <h2>Services</h2> -->
-            <br>
-            <p> <em> <strong>Coming Soon !!!</strong></em></p><br>
-          </div><!-- End Section Title -->
-    </section>
+    <!-- Blog Posts Section -->
+    <section id="blog-posts" class="blog-posts section">
+
+      <div class="container">
+        <div class="row gy-4">
+
+          <div class="col-lg-4"  style="padding-top: 10px">
+            <article class="position-relative h-100">
+
+              <div class="post-img position-relative overflow-hidden">
+                <img src="{{asset('main/assets/img/blog/news1.jpg')}} " class="img-fluid" alt="">
+                <span class="post-date">12 - 16 August 2024</span>
+              </div>
+
+              <div class="post-content d-flex flex-column">
+
+                <h3 class="post-title">The 2nd Ruforum Triennial Conference</h3>
+
+                <div class="meta d-flex align-items-center">
+                  <div class="d-flex align-items-center">
+                    <i class="bi bi-geo-alt"></i> <span class="ps-2">Windhoek, Namibia</span>
+                  </div>
+                  <!-- <span class="px-3 text-black-50">/</span>
+                  <div class="d-flex align-items-center">
+                    <i class="bi bi-hash"></i> <span class="ps-2">RUFtriennial24</span>
+                  </div> -->
+                </div>
+
+                <p style="text-align:justify;">
+                  <strong>Operationalising Higher Agricultural Education and Research Ecosystems for Innovation, Industrialisation and Economic Development in Africa: A Call for Action;</strong> The 2nd RUFORUM Triennial Conference will gather stakeholders like policymakers and academics to discuss key agricultural and economic issues in Africa.
+                </p>
+
+                <hr>
+
+                <a href="https://www.ruforum.org/Triennial2024/" class="readmore stretched-link"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+
+              </div>
+
+            </article>
+          </div><!-- End post list item -->
+
+          
+        </div>
+      </div>
+
+    </section><!-- /Blog Posts Section -->
   </main>
 
   @endsection
--- resources/views/front/projects/achievements.blade.php
+++ resources/views/front/projects/achievements.blade.php
@@ -21,15 +21,72 @@
 
     <!-- Alt Services Section -->
             <!-- Section Title -->
+            <section id="projects" class="projects section">
+
+              <!-- Section Title -->
+              <div class="container section-title" data-aos="fade-up" style="padding-top: 20px">
+                <br>
+              <h2>Achievements</h2>
+              <p>Here are images showcasing our achievements and the places we've visited to promote clean energy solutions and climate action in Africa.</p>
+            </div><!-- End Section Title -->
+              <div class="container">
         
-    <section id="alt-services" class="alt-services section">
-        <div class="container section-title" data-aos="fade-up">
-            <!-- <h2>Services</h2> -->
-            <br>
-            <br>
-            <p> <em> <strong>Coming Soon !!!</strong></em></p><br>
-          </div><!-- End Section Title -->
-    </section>
+                <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+        
+                  <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
+        
+                    <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-remodeling">
+                      <div class="portfolio-content h-100">
+                        <img src="{{asset('main/assets/img/achievement/brussels_01.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                          <h4>2024</h4>
+                          <p>Meeting at Brussels March 2024</p>
+                          <a href="{{asset('main/assets/img/achievement/brussels_01.jpg')}}" title="Brussels 2024" data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                          <!-- <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                        </div>
+                      </div>
+                    </div><!-- End Portfolio Item -->
+        
+                    <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-construction">
+                      <div class="portfolio-content h-100">
+                        <img src="{{asset('main/assets/img/achievement/brussels_02.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                          <h4>2024</h4>
+                          <p>Meeting at Brussels March 2024</p>
+                          <a href="{{asset('main/assets/img/achievement/brussels_02.jpg')}}" title="Brussels 2024" data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                          <!-- <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                        </div>
+                      </div>
+                    </div><!-- End Portfolio Item -->
+                    <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-construction">
+                      <div class="portfolio-content h-100">
+                        <img src="{{asset('main/assets/img/achievement/brussels_03.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                          <h4>2024</h4>
+                          <p>Meeting at Brussels March 2024</p>
+                          <a href="{{asset('main/assets/img/achievement/brussels_03.jpg')}}" title="Brussels 2024" data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                          <!-- <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                        </div>
+                      </div>
+                    </div><!-- End Portfolio Item -->
+                    <div class="col-lg-4 col-md-6 portfolio-item isotope-item filter-construction">
+                      <div class="portfolio-content h-100">
+                        <img src="{{asset('main/assets/img/achievement/brussels_04.jpg')}}" class="img-fluid" alt="">
+                        <div class="portfolio-info">
+                          <h4>2024</h4>
+                          <p>Meeting at Brussels March 2024</p>
+                          <a href="{{asset('main/assets/img/achievement/brussels_04.jpg')}}" title="Brussels 2024" data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                          <!-- <a href="project-details.html" title="More Details" class="details-link"><i class="bi bi-link-45deg"></i></a> -->
+                        </div>
+                      </div>
+                    </div><!-- End Portfolio Item -->
+                  </div><!-- End Portfolio Container -->
+        
+                </div>
+        
+              </div>
+        
+            </section>
   </main>
 
   @endsection
